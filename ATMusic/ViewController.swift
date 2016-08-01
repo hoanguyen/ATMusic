2497b2e027c2f5ff79f9286e019a6b226fa814e3
HoaNV <hoanv@asiantech.vn>
2016-08-01 16:36:00 +0700

    first commit

---
+++ ATMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATMusic
+//
+//  Created by AsianTech on 8/1/16.
+//  Copyright © 2016 at. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
