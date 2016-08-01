141d97a4ac77a12afc4ae77d8ff1d6867d715655
Nguyen Thanh Su <vnntsu@gmail.com>
2016-08-01 17:41:46 +0700

    re-init project and install pod, consist of Alamofire, ObjectMapper, SwiftUtils

---
+++ ATMusic/Controllers/ChartViewController/ChartViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChartViewController.swift
+//  ATMusic
+//
+//  Created by Nguyen Thanh Su on 8/1/16.
+//  Copyright © 2016 at. All rights reserved.
+//
+
+import UIKit
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
